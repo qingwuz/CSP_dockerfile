e62e0f3083d202813e9d85f6245390ed285e1c9a
qingwuz <37654692+qingwuz@users.noreply.github.com>
2019-05-08 11:23:36 +0800

    Update non_attack.py

--- non_attack.py
+++ non_attack.py
@@ -151,8 +151,8 @@ def non_target_graph(x, y, i, x_max, x_min, grad):
   y = first_round * pred + (1 - first_round) * y
   one_hot = tf.one_hot(y, num_classes)
   ########################
-  #logits = (end_points_inc_v1['Logits'] + end_points_res_v1_50['logits'] + end_points_vgg_16['logits']) / 3.0
-  logits = end_points_inc_v1['Logits'] + end_points_res_v1_50['logits'] + end_points_vgg_16['logits']
+  logits = (end_points_inc_v1['Logits'] + end_points_res_v1_50['logits'] + end_points_vgg_16['logits']) / 3.0
+  #logits = end_points_inc_v1['Logits'] + end_points_res_v1_50['logits'] + end_points_vgg_16['logits']
   cross_entropy = tf.losses.softmax_cross_entropy(one_hot,
                                                   logits,
                                                   label_smoothing=0.0,
